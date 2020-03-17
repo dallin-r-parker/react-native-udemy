a87c916efc47154e2dd1fc077daacd216ec71e65
dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-03-17 03:12:51 -0400

    added List Screen

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => <Text>List Screen</Text>;
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
