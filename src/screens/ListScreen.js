735b4f24cce9cd41f1844b872e9755ef1b8e736a
dallin parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-03-17 03:26:35 -0400

    working with FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-const ListScreen = () => <Text>List Screen</Text>;
+const ListScreen = () => {
+  const friends = [
+    { name: "Friend #1", key: "1" },
+    { name: "Friend #2", key: "2" },
+    { name: "Friend #3", key: "3" },
+    { name: "Friend #4", key: "4" },
+    { name: "Friend #5", key: "5" },
+    { name: "Friend #6", key: "6" },
+    { name: "Friend #7", key: "7" },
+    { name: "Friend #8", key: "8" },
+    { name: "Friend #9", key: "9" },
+    { name: "Friend #10", key: "10" }
+  ];
 
-const styles = StyleSheet.create({});
+  return (
+    <FlatList
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={({ name }) => name}
+      data={friends}
+      renderItem={({ item }) => (
+        <Text style={styles.textStyle}>{item.name}</Text>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
