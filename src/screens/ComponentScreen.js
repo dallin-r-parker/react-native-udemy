dbfbc74b5a487fa98ead562ad014ff0c9692254f
Dallin Parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-08-21 14:28:56 -0400

    added styled components and refactored ComponentScreen styles

--- src/screens/ComponentScreen.js
+++ src/screens/ComponentScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
+import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 
 const ComponentScreen = () => {
-  return <Text style={styles.textStyle}>Howdy from ComponentScreen</Text>;
+  return <Title>Howdy from ComponentScreen</Title>;
 };
 
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 20,
-  },
-});
+const Title = styled.Text`
+  color: red;
+  text-align: center;
+  font-size: 16px;
+`;
+
+// const styles = StyleSheet.create({
+//   textStyle: {
+//     fontSize: 20,
+//   },
+// });
 
 export default ComponentScreen;
