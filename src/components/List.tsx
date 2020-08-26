4eb3293c6f9404eb70e0fd907b7ff374fadaf18c
Dallin Parker <13424017+dallin-r-parker@users.noreply.github.com>
2020-08-25 23:28:00 -0400

    List component that uses FlatList - need to better understand units when styling

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import styled from "styled-components/native";
+import { FlatList } from "react-native-gesture-handler";
 import { Text } from "react-native";
 
-export const List: React.FC = () => (
-  <Container>
-    <Text>List component</Text>
-  </Container>
-);
+// NOTE: FlatList react-native docs https://tinyurl.com/y4cg2lxp
 
-const Container = styled.View`
-  flex: 1;
-  color: white;
-  background-color: lightblue;
-  align-items: center;
-  justify-content: center;
+export const List: React.FC = () => {
+  // NOTE: alternatively to using the 'key' property is the "keyExtractor" prop on <FlatList />
+  // the downside to the "keyExtractor" is that it's happening at runtime as opposed to in advance
+  // "key" property must be UNIQUE & String
+  const friends = [
+    { name: "Mikayda", key: "1" },
+    { name: "Trevor", key: "2" },
+    { name: "Hayley", key: "3" },
+    { name: "Cameron", key: "4" },
+  ];
+  return (
+    <>
+      <Text>Here is my Friend List</Text>
+      <FlatList
+        data={friends}
+        renderItem={({ item }) => <Friend>{item.name}</Friend>}
+      />
+    </>
+  );
+};
+
+const Friend = styled.Text`
+  margin-vertical: 25;
+  height: 30;
+  width: 100;
+  text-align: center;
+  border: 3px solid lightblue;
 `;
